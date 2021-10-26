dc714601b9b6a997bef21c9a6619b5f1c60e96ea
mytran123 <mytran091196@gmai.com>
2021-10-26 13:43:39 +0700

    bài tập

--- BT_PhoneNumber/index.php
+++ BT_PhoneNumber/index.php
@@ -1,7 +1,12 @@
 <?php
 //(xx)-(0xxxxxxxxx)
-$pattern = '/^\d{2}\-\[0-9]{10}+$/';
-$subject = '';
+$pattern = '/^\([0-9]{2}\)-\(0[0-9]{9}\)$/';
+$subject1 = '(84)-(0978489648)';
+$subject2 = '(a8)-(22222222)';
 
-$result = preg_match($pattern,$subject);
+$result = preg_match($pattern,$subject1);
 echo $result;
+echo "<br>";
+$result = preg_match($pattern,$subject2);
+echo $result;
+echo "<br>";
